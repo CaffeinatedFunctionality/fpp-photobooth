cc6936eaae9dc0d47af91a669ac18ffd4447ed35
Taylor Mitchell <tmitchell@governmentcio.com>
2022-04-06 17:43:17 -0400

    testing plugin

--- scripts/fpp_install.sh
+++ scripts/fpp_install.sh
@@ -4,9 +4,7 @@
 echo "Running fpp-photobooth Install Script"
 
 sudo apt-get update
-sudo apt-get install -y python-imaging
-sudo apt-get install -y python-imaging-tk
-sudo apt-get install -y python-gdata
+sudo apt-get install -y python-imaging python-imaging-tk python-gdata
 sudo pip install --upgrade google-api-python-client
 sudo pip install --upgrade oauth2client
 sudo apt-get install -y imagemagick
